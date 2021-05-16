d2746474537a9a3c327ca14446ed58cb7742b23a
Joon Kang <joonkang@Joons-MacBook-Pro.local>
2021-05-16 12:10:44 +0900

    infinite scroll

--- stock_view/test/widget_test.dart
+++ stock_view/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:stock_view/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(StockViewer());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
